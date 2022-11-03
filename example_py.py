a5e677b9909d74a0f5ac712db5984044ccba2988
godisboom <guxukai365@gmail.com>
2022-11-03 17:26:36 +0800

    test in py

---
+++ example_py.py
@@ -0,0 +1,13 @@
+# GPU/TensorRT部署参考 examples/vision/detection/paddledetection/python
+import cv2
+import fastdeploy.vision as vision
+
+model = vision.detection.PPYOLOE("ppyoloe_crn_l_300e_coco/model.pdmodel",
+                                 "ppyoloe_crn_l_300e_coco/model.pdiparams",
+                                 "ppyoloe_crn_l_300e_coco/infer_cfg.yml")
+im = cv2.imread("000000014439.jpg")
+result = model.predict(im.copy())
+print(result)
+
+vis_im = vision.vis_detection(im, result, score_threshold=0.5)
+cv2.imwrite("vis_image.jpg", vis_im)
