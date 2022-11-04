ff93322b58b0e51e0dac48155aea6cf85b0b2636
godisboom <guxukai365@gmail.com>
2022-11-04 15:32:14 +0800

    add cpp test

---
+++ example_cpp.cpp
@@ -0,0 +1,17 @@
+// For GPU/TensorRT deployment, please refer to examples/vision/detection/paddledetection/cpp
+#include "fastdeploy/vision.h"
+
+int main(int argc, char* argv[]) {
+  namespace vision = fastdeploy::vision;
+  auto model = vision::detection::PPYOLOE("ppyoloe_crn_l_300e_coco/model.pdmodel",
+                                          "ppyoloe_crn_l_300e_coco/model.pdiparams",
+                                          "ppyoloe_crn_l_300e_coco/infer_cfg.yml");
+  auto im = cv::imread("000000014439.jpg");
+
+  vision::DetectionResult res;
+  model.Predict(&im, &res);
+
+  auto vis_im = vision::Visualize::VisDetection(im, res, 0.5);
+  cv::imwrite("vis_image.jpg", vis_im);
+  return 0;
+ }
